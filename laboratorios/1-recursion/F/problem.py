7a8125e70dd62e569aac7cc0dc5d1fc1d57fc700
Willvel <46783878+Willvel@users.noreply.github.com>
2019-01-20 20:53:15 -0500

    Update problem.py

--- laboratorios/1-recursion/F/problem.py
+++ laboratorios/1-recursion/F/problem.py
@@ -1,11 +1,18 @@
 import json
-
-
+def cambio(num,ori,des,mid):
+    if num >= 1:
+        cambio(num-1,ori,mid,des)
+        impre(ori,des)
+        cambio(num-1,mid,des,ori)
+def impre(a,b):
+    print("mover disco de",a,"a",b)
 # TODO Complete!
 def hanoi(n):
-    return []
-
-
+    num = n
+    ori = "A"
+    des = "B"
+    mid = "C"
+    cambio(num,ori,des,mid)
 if __name__ == '__main__':
     with open('./data.json') as f:
         tests = json.load(f)
