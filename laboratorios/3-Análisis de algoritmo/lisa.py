98be4bf2304ea668bfebe9bc3a98f7e9fc8294f1
Willvel <46783878+Willvel@users.noreply.github.com>
2019-02-05 20:56:47 -0500

    Create lisa.py

---
+++ laboratorios/3-Análisis de algoritmo/lisa.py
@@ -0,0 +1,20 @@
+from sys import stdin
+
+def rec(n,lisa,a,m):
+    if len(lisa) > 0:
+        if lisa[0] > m:
+            m = lisa[0]
+        if lisa[0] < a:
+            a = lisa[0]
+        return rec(n,lisa[1:],a,m)
+    else:
+        print(a,m)
+    
+
+def main():
+    n = int(stdin.readline().strip())
+    lisa = list(map(int,stdin.readline().strip().split()))
+    mini = 0
+    maxi = 0
+    rec(n,lisa,mini,maxi)
+main()
