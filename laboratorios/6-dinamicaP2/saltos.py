1ef932b45b5e9e1671e5da0b68fb71df58980957
Willvel <46783878+Willvel@users.noreply.github.com>
2019-03-17 23:11:32 -0500

    Update saltos.py

--- laboratorios/6-dinamicaP2/saltos.py
+++ laboratorios/6-dinamicaP2/saltos.py
@@ -1,20 +1,27 @@
 from sys import stdin
 def rec(lisa, pos, res, vel):
-    if pos < len(lisa) and lisa[pos]:
-        res.append(pos)
+    print("posi",pos)
+    if pos == len(lisa)-1 and lisa[pos]:
+        print(res)
+        return True
+    elif pos < len(lisa)-1 and lisa[pos]:
+        print("posible",pos)
+        print("caminos",pos+vel-1,pos+vel,pos+vel+1)
+        if pos not in res:
+            res.append(pos)
         return rec(lisa, pos+vel-1, res, vel), rec(lisa, pos+vel, res, vel), rec(lisa, pos+vel+1, res, vel)
     else:
-        if pos == len(lisa) and lisa[pos-1]:
-            return True
-        else:
-            return False
+        print("no",pos)
+        return False
 def main():
     lisa = eval(stdin.readline().strip())
     vel = int(stdin.readline().strip())
     pos = int(stdin.readline().strip())
     res = []
-    if rec(lisa, pos, res, vel):
-        print("True")
+    val = rec(lisa, pos, res, vel)
+    if val:
+        print("correcto",res)
     else:
-        print("False")
+        print("fail")
 main()
+
